637865dbcb4069f0f67a8adf008fa5f16a59ed36
Ivan D Vasin <nisavid@gmail.com>
2014-05-18 22:15:18 -0400

    update Vim session aliases

--- .bash/77-nisavid.sh
+++ .bash/77-nisavid.sh
@@ -11,12 +11,10 @@
 alias ipy='ipython'
 
 # Vim sessions
-alias vapb="vim '+SessionOpen ansible-playbooks'"
 alias vbf="vim '+SessionOpen bedframe'"
 alias vbfa="vim '+SessionOpen bedframe-auth'"
 alias vdl="vim '+SessionOpen deli'"
 alias vnp="vim '+SessionOpen napper'"
-alias vpsl="vim '+SessionOpen px-shortlinks'"
 alias vscoll="vim '+SessionOpen spruce-collections'"
 alias vsdb="vim '+SessionOpen spruce-db'"
 alias vsdt="vim '+SessionOpen spruce-datetime'"
