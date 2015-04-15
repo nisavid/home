e5a9319f77273ab08569fda7316dd45bdba2cba2
Ivan D Vasin <nisavid@philo.com>
2015-04-15 16:28:37 -0400

    Shell: PATH: work around buggy RVM activation

--- .sh/79-rbenv.sh
+++ .sh/79-rbenv.sh
@@ -8,4 +8,8 @@ pathmunge "$HOME/.rvm/bin" after
 # interactive shell -----------------------------------------------------------
 
 
+# remove RVM bin paths because RVM expects them to be at the beginning of PATH
+# but doesn't move them there when it's activated
+export PATH="$(echo :$PATH | sed "s|:$HOME/\.rvm[^:]*/bin||g" | tail -c +2)"
+
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
