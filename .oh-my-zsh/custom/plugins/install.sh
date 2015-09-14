a0ff1400236e61a10906afae8c0d3ce460fa4907
Ivan D Vasin <nisavid@gmail.com>
2015-09-13 22:15:39 -0400

    Oh-My-Zsh custom plugins install script: improve messages, style

--- .oh-my-zsh/custom/plugins/install.sh
+++ .oh-my-zsh/custom/plugins/install.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 # Install Oh-My-Zsh custom plugins via Git
 #
@@ -8,22 +8,22 @@
 # script clones each URI in the user's Oh-My-Zsh custom plugins
 # directory.
 
-src="$(pwd)/$(dirname "$0")"
+src_root="$($(dirname $0)/../../../bin/sh-readlink -f $(dirname $0))"
 
-OMZ_CUSTOM_PLUGINS_DIR="$HOME"/.oh-my-zsh/custom/plugins
+dest_root="$HOME"/.oh-my-zsh/custom/plugins
 
-mkdir -p "$OMZ_CUSTOM_PLUGINS_DIR" \
- && pushd "$OMZ_CUSTOM_PLUGINS_DIR" > /dev/null \
- && { find "$src" -mindepth 1 -maxdepth 1 -type d \
-       | while read plugin_dir; do
-             echo $plugin_dir
-             plugin_name="$(basename "$plugin_dir")"
-             if [[ -f "$plugin_dir"/git_remote_uri \
-                   && ! -e "$plugin_name" ]]; then
-                 git clone \
-                     "$(cat "$plugin_dir"/git_remote_uri | tr -d '\r\n')" \
-                     "$plugin_name"
-             fi
-         done
-      popd > /dev/null
-    }
+mkdir -p "$dest_root" || exit 1
+pushd "$dest_root" > /dev/null \
+  && { find "$src_root" -mindepth 1 -maxdepth 1 -type d \
+         | while read plugin_dir; do
+               plugin_name="$(basename "$plugin_dir")"
+               echo "installing plugin $plugin_name" >&2
+               if [ -f "$plugin_dir"/git_remote_uri ] \
+                  && [ ! -e "$plugin_name" ]; then
+                   git clone \
+                       "$(cat "$plugin_dir"/git_remote_uri | tr -d '\r\n')" \
+                       "$plugin_name"
+               fi
+           done
+       popd > /dev/null
+     }
