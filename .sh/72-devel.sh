85d7b7664b88e97688ffd740a0c314aaa9c36681
Ivan D Vasin <nisavid@gmail.com>
2016-09-10 19:08:08 +0000

    shell: devel config: improve style

--- .sh/72-devel.sh
+++ .sh/72-devel.sh
@@ -7,11 +7,8 @@ export ANSIBLE_HOSTS='/usr/local/bin/ansible-ec2'
 export EC2_HOME='/usr/local/ec2'
 path_affix pre "$EC2_HOME/bin"
 export MYSQL_PS1="\u@\h \d> "
-export RAILS_ENV='development'
-export RI='--format ansi'
-export RIPDIR='/home/nisavid/.rip'
-export RUBYLIB="$RUBYLIB:$RIPDIR/active/lib"
-path_affix post "$RIPDIR/active/bin"
+
+# Python environment ----------------------------------------------------------
 
 PYENVS="$(printf \
     '%s:%s:%s:%s:%s' \
@@ -20,6 +17,14 @@ PYENVS="$(printf \
     "$HOME"/.pyenv/py2devel \
     "$HOME"/.pyenv/py2prod \
     "$PYENVS")"
+
+# Ruby environment ------------------------------------------------------------
+
+export RAILS_ENV='development'
+export RI='--format ansi'
+export RIPDIR='/home/nisavid/.rip'
+export RUBYLIB="$RUBYLIB:$RIPDIR/active/lib"
+path_affix post "$RIPDIR/active/bin"
 
 # Shell option helpers --------------------------------------------------------
 
