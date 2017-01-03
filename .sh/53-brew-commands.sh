9cccea7ff0d3417f79caf836e2639791966293ea
Ivan D Vasin <nisavid@gmail.com>
2017-01-03 19:48:00 +0000

    Shell: Add alias bubu

---
+++ .sh/53-brew-commands.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# Shell configuration | Homebrew/Linuxbrew commands
+
+[ -t 0 ] || return
+# Interactive shell -----------------------------------------------------------
+
+alias bubu='bubo && bubc && brew doctor'
