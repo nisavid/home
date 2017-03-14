8b97db0427d8dc67a91f5970e4895b14b9d9c2c3
Ivan D Vasin <nisavid@gmail.com>
2017-03-14 18:14:40 +0000

    Shell: Fix ls args

---
+++ .sh/50-unix-commands.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# Shell configuration | Unix commands
+
+[ -t 0 ] || return
+# Interactive shell -----------------------------------------------------------
+
+# shellcheck disable=SC2034
+LS_ARGS=''
