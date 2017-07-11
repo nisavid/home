62b5dbfc9b6d2ae88371456bffd7524d8e088743
Ivan D Vasin <ivan@cloudhealthtech.com>
2017-07-11 21:28:17 +0000

    Shell: Add cmd app_open

--- .sh/10-core-commands.sh
+++ .sh/10-core-commands.sh
@@ -6,3 +6,15 @@ if ! which realpath >/dev/null 2>&1; then
         printf %s "$1" | command grep -q '^/' && printf %s "$1" || printf %s "$PWD/${1#./}"
     }
 fi
+
+if ! which app_open >/dev/null 2>&1; then
+    if which xdg-open >/dev/null 2>&1; then
+        app_open() {
+            xdg-open "$@"
+        }
+    elif [ "$(uname -s)" = 'Darwin' ] && which open >/dev/null 2>&1; then
+        app_open() {
+            open "$@"
+        }
+    fi
+fi
