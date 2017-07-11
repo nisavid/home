a83202db9da5280243f0ade3f0932e9e75972fae
Ivan D Vasin <ivan@cloudhealthtech.com>
2017-07-11 21:27:37 +0000

    Shell: Replace realpath only if previously undefined

--- .sh/10-core-commands.sh
+++ .sh/10-core-commands.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Shell configuration | core commands
 
-if ! command -v realpath >/dev/null 2>&1; then
+if ! which realpath >/dev/null 2>&1; then
     realpath() {
         printf %s "$1" | command grep -q '^/' && printf %s "$1" || printf %s "$PWD/${1#./}"
     }
